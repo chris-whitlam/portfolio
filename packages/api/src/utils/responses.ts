25788426d655c8d2b2dfdbf7af99e5c90875ecdf
Chris Whitlam <chris.j.whitlam@gmail.com>
2022-07-31 17:57:17 +0100

    Tidy up backend code

--- packages/api/src/utils/responses.ts
+++ packages/api/src/utils/responses.ts
@@ -1,15 +1,19 @@
-const errorResponse = (message = 'Something went wrong', statusCode = 500) => ({
-  statusCode,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    'Access-Control-Allow-Methods': '*',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    error: message
-  })
-});
+const errorResponse = (message = 'Something went wrong', statusCode = 500) => {
+  console.error(message);
+
+  return {
+    statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Methods': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      error: message
+    })
+  };
+};
 
 export const successResponse = (data: unknown, statusCode = 200) => ({
   statusCode,
@@ -22,17 +26,8 @@ export const successResponse = (data: unknown, statusCode = 200) => ({
   body: JSON.stringify(data)
 });
 
-export const badRequest = (message: string) => {
-  console.log(message);
-  return errorResponse(message, 400);
-};
-
-export const internalServerError = (message = 'Something went wrong') => {
-  console.log(message);
-  return errorResponse(message, 500);
-};
-
-export const unprocessableEntity = (message = 'Something went wrong') => {
-  console.log(message);
-  return errorResponse(message, 422);
-};
+export const badRequest = (message: string) => errorResponse(message, 400);
+export const internalServerError = (message = 'Something went wrong') =>
+  errorResponse(message, 500);
+export const unprocessableEntity = (message = 'Something went wrong') =>
+  errorResponse(message, 422);
