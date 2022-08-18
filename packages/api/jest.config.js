fffb739e913f74c55ad14ebe5f2932665d328bd7
Chris Whitlam <chris.j.whitlam@gmail.com>
2022-08-18 15:20:18 +0100

    Add package.json commands

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -9,11 +9,19 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    }
+  },
   testMatch: ['**/*.(unit|integration).test.ts'],
   testEnvironment: 'node',
   reporters: ['default'],
   setupFiles: ['./jest.setup.js'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
   moduleFileExtensions: ['ts', 'js'],
   moduleNameMapper: {
     '@utils': '<rootDir>/src/utils',
